b35068278a549201052fb64ec8e050d364763f53
Ddaudiosolutions <ddaudiosolutions@gmail.com>
2021-06-13 16:15:27 +0200

    cambios de host port

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.use ((err, req, res, next)=>{
 })
 
 //LEER LOCALHOST DE VARIABLES Y PORT
-const host = process.env.HOST;
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 4000;
 
 //ESCUCHANDO EL PUERTO
